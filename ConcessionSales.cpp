278bcc16484334c3bd1d1980a8fdd84e6887ff6f
Cameron Hattendorf <hattendorfcameron@gmail.com>
2020-02-24 08:33:09 -0800

    Saved concessionSales

---
+++ ConcessionSales.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+int main(){
+    double candySales,
+        popcornSales,
+        drinkSales,
+        candyPrice,
+        popcornPrice,
+        drinkPrice,
+        //the below variables were missing from algorithm
+        grossCandy,
+        grossPopcorn,
+        grossDrinks,
+        totalSales;
+    ifstream 
+
+    //file use missing from IPO
+    //assign sale prices from file
+    ifstream
+    infile.open(ConcessionPrices.txt)
+
+    //Prompt/Get sold
+    cout << "How many candy sales? ";
+    cin >> candySales;
+    cout << "How many popcorn sales? ":
+    cin >> popcornSales;
+    cout << "How many drink sales";
+    cin >> drinkSales;
+
+    //close file here
+
+    //process sales
+
+    grossCandy = candyPrice * candySales;
+    grossPopcorn = popcornPrice * popcornSales;
+    grossDrinks = drinkPrice * drinkSales;
+
+    totalSales = grossCandy + grossDrinks + grossPopcorn;
+
+    //Display gross sales by in order of dollar amount and total
+
+    //if else statements to organize dollar amounts
+
+    cout << totalSales
+
+    return 0
+}
